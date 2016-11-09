e8ff83a45964771c4e6a959959dd6eb762837032
Joni Mertoniemi <joni.mertoniemi@sysart.fi>
2016-11-09 09:40:10 +0200

    Add custom fields

--- wp/wp-content/themes/sysart/functions.php
+++ wp/wp-content/themes/sysart/functions.php
@@ -6,14 +6,3 @@ foreach (glob(__DIR__ . '/functions/*.php') as $file) {
 }
 
 add_theme_support('post-thumbnails');
-
-// XXX: do we need these? no?
-//
-// add_filter('json_prepare_post', 'json_api_encode_acf');
-// add_filter( 'acf/rest_api/types', function( $types ) {
-//     if ( array_key_exists( 'post', $types ) ) {
-//         unset( $types['post'] );
-//     }
-//
-//     return $types;
-// } );
