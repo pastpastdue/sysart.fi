8c312b2f8c51b8c1034a69581011d8a45d34f457
Joni Mertoniemi <joni.mertoniemi@sysart.fi>
2016-11-11 08:50:23 +0200

    Style stuff

--- wp/wp-content/themes/sysart/page-jobs.php
+++ wp/wp-content/themes/sysart/page-jobs.php
@@ -7,10 +7,10 @@
 get_header();
 $fields = get_fields();
 ?>
-<section class="content-section">
+<section class="content-block">
   <div class="row large-gutter">
     <div class="col-sm-6 col-sm-push-6 col-md-4 col-md-push-8">
-      <?php the_post_thumbnail(null, array('class' => 'img-responsive')); ?>
+      <?php the_post_thumbnail(array(500), array('class' => 'img-responsive post-image')); ?>
     </div>
     <div class="col-sm-6 col-sm-pull-6 col-md-8 col-md-pull-4">
       <div class="content post-content">
@@ -18,7 +18,11 @@ $fields = get_fields();
       </div>
     </div>
   </div>
+</section>
+<section class="content-block">
   <?php echo new Benefits($fields['benefits']); ?>
+</section>
+<section class="content-block">
   <div class="row text-center">
     <h2><?php echo $fields['jobs_title']; ?></h2>
   </div>
--- wp/wp-content/themes/sysart/single-job.php
+++ wp/wp-content/themes/sysart/single-job.php
@@ -1,16 +1,14 @@
 <?php
 get_header();
 ?>
-<section class="content-section">
-  <div class="bold-title">
-    <h1><?php echo $post->post_title; ?></h1>
-  </div>
+<section class="content-block">
   <div class="row large-gutter">
     <div class="col-sm-6 col-sm-push-6">
-      <?php the_post_thumbnail(null, array('class' => 'img-responsive')); ?>
+      <?php the_post_thumbnail(array(600), array('class' => 'img-responsive post-image')); ?>
     </div>
     <div class="col-sm-6 col-sm-pull-6">
       <div class="content post-content">
+        <h1><?php echo $post->post_title; ?></h1>
         <?php echo apply_filters('the_content', $post->post_content); ?>
       </div>
     </div>
