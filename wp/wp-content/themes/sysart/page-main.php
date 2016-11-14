79055055897ad1adb0123a461fd8fbee6437db9e
Joakim Granbohm <joakim.granbohm@sysart.fi>
2016-11-14 16:21:00 +0200

    change frontpage structure and ad alt-bg-class

--- wp/wp-content/themes/sysart/page-main.php
+++ wp/wp-content/themes/sysart/page-main.php
@@ -20,46 +20,35 @@
 
 </div> <!-- .container-->
 
-<div class="fp-blocks">
-    <div class="fp-block fp-block__jumbotron">
-        <div class="container">
-            <div class="main-container">
-                <div class="row content-section">
-                    <?php echo $jumbotron; ?>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="fp-block fp-block__services">
-        <div class="container">
-            <div class="main-container">
-                <h2 class="fp-block__header">Palvelut</h2>
-                <?php echo $servicelist; ?>
-            </div>
-        </div>
-    </div>
-
-    <div class="fp-block fp-block__clientlist">
-        <div class="container">
-            <div class="main-container">
-                <h2 class="fp-block__header">Asiakkaat</h2>
-                <?php echo $clientslist; ?>
-            </div>
-        </div>
-    </div>
-
-    <div class="fp-block fp-block__twitterfeed">
-        <div class="container">
-            <div class="main-container">
-                <h2 class="fp-block__header">Twitter</h2>
-                <?php echo $twitterwall; ?>
-            </div>
-        </div>
+<div class="container">
+    <div class="content-block">
+        <?php echo $jumbotron; ?>
     </div>
 </div>
 
-<div class="container fp-footer-container">
-    <div class="main-container">
-        <?php
-        get_footer();
+<div class="alt-bg-color">
+    <div class="container">
+        <section class="content-block">
+            <h2 class="">Palvelut</h2>
+            <?php echo $servicelist; ?>
+        </section>
+    </div>
+</div>
+
+<div class="container">
+    <section class="content-block">
+        <h2 class="">Asiakkaat</h2>
+        <?php echo $clientslist; ?>
+    </section>
+</div>
+
+<div class="alt-bg-color">
+    <div class="container">
+        <section class="content-block">
+            <h2 class="">Twitter</h2>
+            <?php echo $twitterwall; ?>
+        </section>
+    </div>
+</div>
+
+<?php get_footer();
