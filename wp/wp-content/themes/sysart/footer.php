f8113e6b755dae1d2e9b49dbb3b74b131b2653fb
Joni Mertoniemi <joni.mertoniemi@sysart.fi>
2016-11-17 14:10:16 +0200

    Fix footer

--- wp/wp-content/themes/sysart/footer.php
+++ wp/wp-content/themes/sysart/footer.php
@@ -1,22 +1,22 @@
 <?php
-setup_postdata(get_post(get_option('page_on_front')));
+$footer = get_field('footer', get_option('page_on_front'));
 ?>
     </div><? // close wrapper ?>
     <footer>
       <div id="footer-wrapper">
         <div class="block row no-gutter">
-          <?php while(have_rows('footer')): the_row(); ?>
+          <?php foreach ($footer as $element): ?>
             <div class="item col-sm-3">
               <div class="item__content">
-                <?php if (get_row_layout() == 'address'): ?>
-                  <div class="title title--small"><?php the_sub_field('title'); ?></div>
-                  <address><?php the_sub_field('text'); ?></address>
+                <?php if ($element['acf_fc_layout'] == 'address'): ?>
+                  <div class="title title--small"><?php echo $element['title']; ?></div>
+                  <address><?php echo $element['text']; ?></address>
                 <?php else: ?>
-                  <?php echo wp_get_attachment_image(get_sub_field('image')); ?>
+                  <?php echo wp_get_attachment_image($element['image']); ?>
                 <?php endif; ?>
               </div>
             </div>
-          <?php endwhile; ?>
+          <?php endforeach; ?>
           <div class="text-center copyright-info col-xs-12">
             <p class="copyright">&copy; Sysart Oy 2016</p>
           </div>
