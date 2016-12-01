0a8cdb3ba51ad14b62a86bfce1638cf7b7f3c006
Joni Mertoniemi <joni.mertoniemi@sysart.fi>
2016-12-01 14:50:12 +0200

    Improve contact page

--- wp/wp-content/themes/sysart/page-contact.php
+++ wp/wp-content/themes/sysart/page-contact.php
@@ -6,7 +6,10 @@ $fields = get_fields();
 
 the_post();
 
+$footer = get_field('footer', get_option('page_on_front'));
+
 $peoples_list = new PeoplesList(get_field('employees'));
+
 
 get_header();
 ?>
@@ -14,6 +17,19 @@ get_header();
   <div class="block__content">
     <h1 class="hero__title"><?php the_title(); ?></h1>
   </div>
+</div>
+<div class="block row no-gutter text text--small">
+  <?php foreach ($footer as $element): if ($element['acf_fc_layout'] === 'address'): ?>
+    <section class="col-md-4">
+      <div class="item item--center">
+        <div class="item__content">
+          <?php echo wp_get_attachment_image($element['image'], 'thumbnail', false, array('class' => 'item__image')); ?>
+          <div class="title title--small"><?php echo $element['title']; ?></div>
+          <address class="address"><?php echo $element['text']; ?></address>
+        </div>
+      </div>
+    </section>
+  <?php endif; endforeach; ?>
 </div>
 <?php echo $peoples_list; ?>
 <?php get_footer(); ?>
