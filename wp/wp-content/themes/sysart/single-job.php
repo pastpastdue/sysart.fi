f05172cc4148fea5b3012c25d6cbf5db9563cbe3
Joni Mertoniemi <joni.mertoniemi@sysart.fi>
2016-12-02 14:40:28 +0200

    Fix jobs

--- wp/wp-content/themes/sysart/single-job.php
+++ wp/wp-content/themes/sysart/single-job.php
@@ -2,20 +2,21 @@
 get_header();
 the_post();
 ?>
-<section class="content-block">
-  <div class="row large-gutter">
-    <div class="col-sm-6 col-sm-push-6">
-      <?php the_post_thumbnail(array(600), array('class' => 'img-responsive post-image')); ?>
-    </div>
-    <div class="col-sm-6 col-sm-pull-6">
-      <div class="content post-content">
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
+<div class="container">
+  <section class="content-block">
+    <div class="row large-gutter">
+      <div class="col-sm-6 col-sm-push-6">
+        <?php the_post_thumbnail(array(600), array('class' => 'img-responsive post-image')); ?>
+      </div>
+      <div class="col-sm-6 col-sm-pull-6">
+        <div class="content post-content">
+          <h1><?php the_title(); ?></h1>
+          <?php the_content(); ?>
+        </div>
       </div>
     </div>
-  </div>
-
-</section>
+  </section>
+</div>
 <?php
 get_footer();
 ?>
