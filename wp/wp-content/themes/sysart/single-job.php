98dd1569b2890e417ad07c32f8f7f2d2c7126b07
Joni Mertoniemi <joni.mertoniemi@sysart.fi>
2017-01-05 14:26:25 +0200

    add job floater

--- wp/wp-content/themes/sysart/single-job.php
+++ wp/wp-content/themes/sysart/single-job.php
@@ -2,20 +2,15 @@
 get_header();
 the_post();
 ?>
-<section class="content-block">
-  <div class="row large-gutter">
-    <div class="col-sm-6 col-sm-push-6">
-      <?php the_post_thumbnail(array(600), array('class' => 'img-responsive post-image')); ?>
-    </div>
-    <div class="col-sm-6 col-sm-pull-6">
-      <div class="content post-content">
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
-      </div>
-    </div>
+<div class="hero block">
+  <div class="block__content block--light">
+    <h1 class="hero__title"><?php the_title(); ?></h1>
   </div>
-
-</section>
-<?php
-get_footer();
-?>
+</div>
+<div class="block block--text wysiwyg">
+  <div class="block__content">
+    <?php the_post_thumbnail(array(600), array('class' => 'image image--floater')); ?>
+    <?php the_content(); ?>
+  </div>
+</div>
+<?php get_footer(); ?>
