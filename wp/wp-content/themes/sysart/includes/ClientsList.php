228db189233aaf763f4d52a865646ad54b080fa3
helinakytonen <helina.kytonen@sysart.fi>
2017-01-11 12:22:29 +0200

    move client titles below client images

--- wp/wp-content/themes/sysart/includes/ClientsList.php
+++ wp/wp-content/themes/sysart/includes/ClientsList.php
@@ -12,10 +12,11 @@ class ClientsList {
       $this->clients .= <<<EOC
 <section class="col-xs-12 col-sm-4 col-md-3">
   <div class="item item--small-margin item--square item--background $background item--hover item--hide-blur">
+    <a href="$url"></a>
+  </div>
+  <div class="people-content no-child-margins text--center">
     <a href="$url">
-      <div class="item__content no-child-margins">
-        {$post->post_title}
-      </div>
+      <h2 class="title title--thinner">{$post->post_title}</h2>
     </a>
   </div>
 </section>
