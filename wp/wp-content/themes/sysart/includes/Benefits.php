81a12bf649d05c12bfd3692bcaf17445edcc7086
helinakytonen <helina.kytonen@sysart.fi>
2017-05-09 12:43:36 +0300

    Add Featured Blogs block to Jobs page and a new acf field (URL) to Benefits

--- wp/wp-content/themes/sysart/includes/Benefits.php
+++ wp/wp-content/themes/sysart/includes/Benefits.php
@@ -9,6 +9,12 @@ class Benefits {
 
     foreach ($this->benefits as $benefit) {
       $image = wp_get_attachment_image($benefit['image'], array(768, 0), false, array('class' => 'item__image item__icon'));
+        $benefit_text = $benefit['benefit'];
+
+        if ($benefit['url']) {
+            $image = '<a href="' . $benefit['url'] . '">' . $image . '</a>' ;
+            $benefit_text = '<a href="' . $benefit['url'] . '">' . $benefit_text . '</a>';
+        };
 
       $benefits .= <<<EOC
         <div class="col-tn-6 col-xs-6 col-sm-3 col-md-3 col-lg-3">
@@ -16,7 +22,7 @@ class Benefits {
               <div class="item__content">
                 $image
                 <div class="title--icon">
-                  {$benefit['benefit']}
+                  $benefit_text
                 </div>
               </div>
           </div>
--- wp/wp-content/themes/sysart/page-jobs.php
+++ wp/wp-content/themes/sysart/page-jobs.php
@@ -11,6 +11,11 @@ $jobs_list = new JobsList($jobs);
 
 $hero_bg = StyleInjector::addBackground(get_post_thumbnail_id());
 
+$featured_blog_list = new BlogList(get_field('featured_blogs', get_option('page_on_front')));
+$blog_intro_title = get_field('blog_intro_title', get_option('page_on_front'));
+$blog_intro_subtitle = get_field('blog_intro_subtitle', get_option('page_on_front'));
+$blog_intro_text = get_field('blog_intro_text', get_option('page_on_front'));
+
 get_header();
 ?>
 <div class="hero block <?php echo $hero_bg; ?>">
@@ -36,4 +41,16 @@ get_header();
   </div>
 </div>
 <?php echo $jobs_list; ?>
+<div class="block block--large">
+  <div class="block__content child-margins">
+    <h2 class="title title--highlight"><?php echo $blog_intro_title; ?></h2>
+    <p class="title title--medium"><?php echo $blog_intro_subtitle; ?></p>
+    <p><?php echo $blog_intro_text; ?></p>
+  </div>
+</div>
+<div class="block block--full-width block--condensed-top">
+  <div class="block__content block__content--condensed-bottom">
+    <?php echo $featured_blog_list; ?>
+  </div>
+</div>
 <?php get_footer(); ?>
