c76ba2addfce15fa7e38c0135f7c6fd97e62d49b
helinakytonen <helina.kytonen@sysart.fi>
2017-05-09 13:37:40 +0300

    Fixed Featured Blogs list: list items no longer identical to those on front page

--- wp/wp-content/themes/sysart/page-jobs.php
+++ wp/wp-content/themes/sysart/page-jobs.php
@@ -11,10 +11,7 @@ $jobs_list = new JobsList($jobs);
 
 $hero_bg = StyleInjector::addBackground(get_post_thumbnail_id());
 
-$featured_blog_list = new BlogList(get_field('featured_blogs', get_option('page_on_front')));
-$blog_intro_title = get_field('blog_intro_title', get_option('page_on_front'));
-$blog_intro_subtitle = get_field('blog_intro_subtitle', get_option('page_on_front'));
-$blog_intro_text = get_field('blog_intro_text', get_option('page_on_front'));
+$featured_blog_list = new BlogList(get_field('featured_blogs'));
 
 get_header();
 ?>
@@ -43,14 +40,22 @@ get_header();
 <?php echo $jobs_list; ?>
 <div class="block block--large">
   <div class="block__content child-margins">
-    <h2 class="title title--highlight"><?php echo $blog_intro_title; ?></h2>
-    <p class="title title--medium"><?php echo $blog_intro_subtitle; ?></p>
-    <p><?php echo $blog_intro_text; ?></p>
+    <?php if (get_field('blog_intro_title')) { ?>
+      <h2 class="title title--highlight"><?php the_field('blog_intro_title'); ?></h2>
+    <?php }; ?>
+    <?php if (get_field('blog_intro_subtitle')) { ?>
+      <p class="title title--medium"><?php the_field('blog_intro_subtitle'); ?></p>
+    <?php }; ?>
+    <?php if (get_field('blog_intro_text')) { ?>
+      <p><?php the_field('blog_intro_text'); ?></p>
+    <?php }; ?>
   </div>
 </div>
-<div class="block block--full-width block--condensed-top">
-  <div class="block__content block__content--condensed-bottom">
-    <?php echo $featured_blog_list; ?>
+<?php if ($featured_blog_list) { ?>
+  <div class="block block--full-width block--condensed-top">
+    <div class="block__content block__content--condensed-bottom">
+      <?php echo $featured_blog_list; ?>
+    </div>
   </div>
-</div>
+<?php }; ?>
 <?php get_footer(); ?>
